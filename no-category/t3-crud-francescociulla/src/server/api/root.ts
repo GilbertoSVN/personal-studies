e1cf38677ebaa46b400b8a95eac1f07f20e6c5f1
GilbertoVN <nvotreblig@gmail.com>
2023-07-13 11:44:07 -0300

    fix: file name

--- no-category/t3-crud-francescociulla/src/server/api/root.ts
+++ no-category/t3-crud-francescociulla/src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { userRouter } from "~/server/api/routers/example";
+import { userRouter } from "~/server/api/routers/user";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 export const appRouter = createTRPCRouter({
